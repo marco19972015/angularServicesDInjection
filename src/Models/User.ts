0877ff1ae999ec64ada00208d1a43164a66c69f8
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-07 12:27:03 -0500

    Created a User model and implemented some logic

---
+++ src/Models/User.ts
@@ -0,0 +1,16 @@
+export class User{
+    // Constructor to be able to create new users
+    constructor(name: string, gender: string, subType: string, status:string){
+        // inside we can assign the properties that will be coming in from the constructor
+        // to the props below the constructor
+        this.name = name;
+        this.gender = gender;
+        this.subType = subType;
+        this.status = status;
+    }
+
+    name: string;
+    gender: string;
+    subType: string;
+    status: string;
+}
