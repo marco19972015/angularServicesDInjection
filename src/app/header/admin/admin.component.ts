1a3824274734026541775a20942de035c9de62eb
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-08 10:57:08 -0500

    Added a new providers array in our Admin component in order to use the CreateUser method. However, in doing so we create a bug that doesn't actually add the new user since we create multiple instances of the providers array dependency injection

--- src/app/header/admin/admin.component.ts
+++ src/app/header/admin/admin.component.ts
@@ -1,13 +1,22 @@
 import { Component, Inject } from '@angular/core';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-admin',
-  templateUrl: './admin.component.html'
+  templateUrl: './admin.component.html',
+  providers: [UserService]
 })
 export class AdminComponent {
+  // In this component we will be adding the new users
+
+  constructor(private userService: UserService){}
 
   name: string = '';
   gender: string = 'Male';
   subType: string = 'Yearly';
   status: string = 'Active';
+
+  createNewUser(){
+    this.userService.CreateUser(this.name, this.gender, this.subType, this.status)
+  }
 }
